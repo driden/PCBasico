ba3d3a2fd678242ca5d60a0551cd2b90247b4e51
driden <lignacio.loureiro@gmail.com>
2018-11-11 20:54:23 -0300

    cambios

--- ada/src/main.adb
+++ ada/src/main.adb
@@ -1,6 +1,27 @@
-procedure Main is
+with Productor; use Productor;
+with Consumidor; use Consumidor;
+--with BufferPC; use BufferPC;
 
+procedure Main is
+procedure StartAll is
+   p1, p2, p3, p4, p5 : TProductor;
+   c1, c2, c3         : TConsumidor;
 begin
-   --  Insert code here.
-   null;
+      p1;
+   p2;
+   p3;
+   p4;
+   p5;
+   c1;
+   c2;
+   c3;
+
+end StartAll;
+begin
+
+
+   StartAll;
+
 end Main;
+
+
